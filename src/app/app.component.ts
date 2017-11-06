ddf6855b599b70d76049b33ac485c8adb5145553
juan carlos guerrero <jcguerrerom21@gmail.com>
2017-11-06 14:01:40 +0100

    UPDATED - SE AÑADE PARTE DE LA PÁGINA DE INICIO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,49 @@ declare var $: any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  imgIcono: string;
+  imgSalida: string;
+  imgFutbol: string;
+  imgFutsal: string;
+  imgBasket: string;
+  imgPadel: string;
+  imgTenis: string;
+  imgAtleta: string;
+  imgNadador: string;
+  imgEquipoBaloncesto: string;
+
+  constructor() {
+    this.imgSalida = 'assets/imagenes/salida.jpg';
+    this.imgIcono = 'assets/imagenes/icono.png';
+    this.imgFutbol = 'assets/imagenes/futbol.jpg';
+    this.imgFutsal =  'assets/imagenes/futsal.jpg';
+    this.imgBasket = 'assets/imagenes/basket.jpg';
+    this.imgPadel = 'assets/imagenes/padel.jpg';
+    this.imgTenis = 'assets/imagenes/tenis1.jpg';
+    this.imgAtleta = 'assets/imagenes/atleta.jpg';
+    this.imgNadador = 'assets/imagenes/nadador.jpg';
+    this.imgEquipoBaloncesto = 'assets/imagenes/baloncesto.jpg';
+  }
 
   ngOnInit() {
     $( document ).ready(function(){
-      $("#btnMenu").sideNav();
+      $('#btnMenu').sideNav(); //botón menú formato responsive
+      $('.slider').slider();
     });
+
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+    //login
+    $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrainWidth: false, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: false, // Displays dropdown below the button
+        alignment: 'left', // Displays dropdown with edge aligned to the left of button
+        stopPropagation: false // Stops event propagation
+      }
+    );
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MaterializeModule } from 'angular2-materialize';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
